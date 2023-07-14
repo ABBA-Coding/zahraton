f4531a89ca138b771f10916f01e7d31f6549260b
BoastfulApex <jahongirnormuminov8@gmail.com>
2023-07-14 16:40:31 +0500

    edited

--- apps/authentication/auth_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from knox import views as knox_views
-from .views import LoginView, PhoneVerify
-
-urlpatterns = [
-     path(r'phone_verify/', PhoneVerify.as_view(), name='phone_verify'),
-     path(r'login/', LoginView.as_view(), name='knox_login'),
-     path(r'logout/', knox_views.LogoutView.as_view(), name='knox_logout'),
-]
