7ec70a1c2f48c1c8c4a7e0c32f45ad2155520016
Kairat <productgsm@gmail.com>
2023-10-12 22:36:22 +0500

    import export adding to telegramUser

--- apps/telegram_bot/admin.py
+++ apps/telegram_bot/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
+from import_export.admin import ExportActionMixin
+
 from .models import TelegramUser, TelegramChat
 
 
-@admin.register(TelegramUser)
+@admin.register(ExportActionMixin, TelegramUser)
 class TelegramUserAdmin(admin.ModelAdmin):
     list_display = ["telegram_id", "phone", "moneyback_id"]
     search_fields = ["phone"]
