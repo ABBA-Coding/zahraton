d7c39a6d92e01e82f2e477026d926a1947d54dfa
Kairat <productgsm@gmail.com>
2023-10-12 22:47:52 +0500

    fix

--- apps/telegram_bot/admin.py
+++ apps/telegram_bot/admin.py
@@ -4,8 +4,8 @@ from import_export.admin import ExportActionMixin
 from .models import TelegramUser, TelegramChat
 
 
-@admin.register(ExportActionMixin, TelegramUser)
-class TelegramUserAdmin(admin.ModelAdmin):
+@admin.register(TelegramUser)
+class TelegramUserAdmin(ExportActionMixin, admin.ModelAdmin):
     list_display = ["telegram_id", "phone", "moneyback_id"]
     search_fields = ["phone"]
 
