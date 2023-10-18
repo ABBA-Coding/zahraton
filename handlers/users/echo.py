c618096fbfc9aff116659c66195e2000c18da850
STQT <productgsm@gmail.com>
2023-10-18 13:36:05 +0500

    update echo

--- handlers/users/echo.py
+++ handlers/users/echo.py
@@ -1,8 +1,12 @@
+from aiogram.dispatcher import FSMContext
+
 from keyboards.inline.menu_button import menu_keyboard
 from loader import dp, bot
 from aiogram import types
 
 
 @dp.message_handler(state=None)
-async def bot_echo(message: types.Message):
+async def bot_echo(message: types.Message, state: FSMContext):
     await message.answer("Bo'limni tanlang 👇", reply_markup=menu_keyboard())
+    await state.set_state('user_menu')
+    await state.update_data(action='menu')
