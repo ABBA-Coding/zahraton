9e7b0ea9cf8fde7b6fd030120b9c077d0ec61d90
STQT <productgsm@gmail.com>
2023-10-19 18:03:51 +0500

    telegramUser list_display show №

--- apps/telegram_bot/admin.py
+++ apps/telegram_bot/admin.py
@@ -3,22 +3,24 @@ from import_export.admin import ExportActionMixin
 from django.db.models import Count
 from .models import TelegramUser, TelegramChat
 
-
+from django.db.models import F
+from django.db.models.expressions import Window
+from django.db.models.functions import DenseRank
 @admin.register(TelegramUser)
 class TelegramUserAdmin(ExportActionMixin, admin.ModelAdmin):
-    list_display = ["serial_number", "telegram_id", "phone", "moneyback_id"]
+    list_display = ["serial_number", "telegram_id", "phone"]
     search_fields = ["phone"]
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
-        queryset = queryset.annotate(serial_number=Count('id'))
+        queryset = queryset.annotate(serial_number=Window(expression=DenseRank(), order_by=F('id').desc()))
         return queryset
 
     def serial_number(self, obj):
         return obj.serial_number
 
     serial_number.short_description = '№'
-    serial_number.admin_order_field = 'id'
+    serial_number.admin_order_field = 'serial_number'
 
 
 @admin.register(TelegramChat)
